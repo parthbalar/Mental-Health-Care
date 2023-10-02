638452e51870d765c80639dac450a93f353090be
parthbalar <134857446+parthbalar@users.noreply.github.com>
2023-10-02 12:18:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,24 +7,24 @@
   window.addEventListener("load",function(){
  
    showPopup();
-   // setTimeout(function(){
-   //   loginPopup.classList.add("show");
-   // },5000)
+   setTimeout(function(){
+   loginPopup.classList.add("show");
+   },5000)
 
   })
 
-  // function showPopup(){
-  //       const timeLimit = 3 // seconds;
-  //       let i=0;
-  //       const timer = setInterval(function(){
-  //        i++;
-  //        if(i == timeLimit){
-  //         clearInterval(timer);
-  //         loginPopup.classList.add("show");
-  //        } 
-  //        console.log(i)
-  //       },1000);
-  // }
+  function showPopup(){
+         const timeLimit = 3 // seconds;
+         let i=0;
+         const timer = setInterval(function(){
+          i++;
+          if(i == timeLimit){
+           clearInterval(timer);
+           loginPopup.classList.add("show");
+          } 
+          console.log(i)
+        },1000);
+   }
 
 
   // close.addEventListener("click",function(){
